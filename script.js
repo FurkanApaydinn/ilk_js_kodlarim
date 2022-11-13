b536d63731fb56fef8d4e279380ecdae1c112bcd
Furkan Apaydın <114331714+FurkanApaydinn@users.noreply.github.com>
2022-11-13 13:13:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let js = "harika";
+if (js === "harika") alert("JS harika bir dil");
+else alert("JS harika bir dil değil");
+console.log(40 + 8 + 23 - 10);
+// console.log("Murat");
+console.log(23);
+let isim = "samet";
+console.log(isim);
+console.log(isim);
+console.log(isim);
+// 1. değişken tanımlamalarda _ ve $ işareti kullanılabilir.
+// 2. Değişkenler rakamla başlamaz örneğin 3adi gibi.
+// let $function olarak tanımlanabilir.
+// Değişken isimleri büyük harfle başlayamaz.
+// Değiken adları anlamlı olması gerekiyor.
+let ilkisim = "mühendis";
+let mevcutisim = "Öğretmen";
+console.log(ilkisim);
+console.log(mevcutisim);
+
+// Örnek challange
+// let ülke = "Türkiye";
+let kıta = "Asya";
+// let nüfus = 6000000;
+// console.log(ülke);
+console.log(kıta);
+// console.log(nüfus);
+let jseglenceli = true;
+// typeof değişken türünü tarayıcada göstermeye yarar.
+console.log(typeof true);
+// console.log(typeof ülke);
+// console.log(typeof nüfus);
+
+// Örnek challange2
+let adaMı = true;
+let dil = "İngilizce";
+let nüfus = 6000000;
+let ülke = "Türkiye";
+console.log(adaMı);
+console.log(typeof dil);
+console.log(typeof nüfus);
+console.log(typeof ülke);
