dca53eb791534990b07bacfb51f67e7c771cd67e
Furkan Apaydın <114331714+FurkanApaydinn@users.noreply.github.com>
2022-12-08 16:55:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,59 @@ const vücutkitleendeksiengin = ağırlıkengin / boyengin ** 2;
 console.log("Enginin vücut kitle endeksi : " + vücutkitleendeksiengin);
 const büyükmü = vücutkitleendeksimurat > vücutkitleendeksiengin;
 console.log(büyükmü);
+
+let numOne=3
+let numTwo=3
+
+console.log(numOne==numTwo)
+
+let js = "javascript"
+let py="Python"
+console.log(js==py)// eşit olmadıkları için false dönecek
+
+let lightOn=true
+let lightOff=false
+console.log(lightOff==lightOn)
+
+let nums=[// dizeler her zaman 0 dan başlar
+    1,2,3,"furkan" 
+]
+nums[0]//1
+nums[1]//2
+nums[2]//3
+nums[3]="Apaydın" // dizede yer alan 3üncü elemanı apaydın olarak değiştiriyorum.
+console.log(nums)
+
+let nums1=[1,2,3]
+let nums2=[1,2,3]
+console.log(nums1==nums2) // eşit olmalarına rağmen non-primitive(ilkel olmayan veri türü) olduğu için false döndürecek.
+// console.log(math.round(5.49))// 5 e yuvarlar
+console.log(Math.round(5.5))// 6 ya yuvarlar
+console.log(Math.ceil(5.01)) // 6 ya yuvarlar tavana(yukarı) yuvarla
+console.log(Math.ceil(5.95)) // 6 ya yuvarlar tavana(yukarı) yuvarla
+console.log(Math.floor(5.99)) // tabana(aşağı) yuvarlar sonuç 5 
+console.log(Math.min(5,3,1,-5,-20))// liste içerisinde en küçük sayıyı bulur.
+console.log(Math.max(5,3,1,-5,-20))// liste içerisinde en büyük sayıyı bulur.
+const num1=Math.random()// 0 ile 1 arasında rastgele bir sayı oluştur. 
+console.log(num1)
+Math.floor(Math.random*11) // 0 ile 10 arasında rastgele sayı oluştur.
+
+let name ='Furkan'
+let surName='Apaydın'
+let fullName= name + surName//furkan apaydın
+
+console.log(`${name} ${surName}`) // furkan apaydın 
+
+//string methods uzunluk
+let ad ="furkanapaydin"
+console.log(ad.length) // 6 uzunluk sayısını gösterir
+'furkan'.toUpperCase() // tüm harfleri büyük yapar
+'furkan'.toLocaleLowerCase() // tüm harfleri küçük yapar
+ad.substring(2,5) // rka 2. indexten başla ve kaç uzunluk alacağını belirtiyoruz.
+let string="javascript"
+console.log(string.substring(0,4))//java
+console.lincog(string.substring(4,10)) // script
+
+console.log(string.includes("java")) // true // string içerisinde java var ise true döndürür. yok ise false döndürür
+
+console.log(string.includes("fur"))//false 
